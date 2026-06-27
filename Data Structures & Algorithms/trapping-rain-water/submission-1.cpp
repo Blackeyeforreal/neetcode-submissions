0ce5269e3ab6e7dd60538f10f947916df1fb20d9
Blackeyeforreal <63438842+Blackeyeforreal@users.noreply.github.com>
2026-06-27 14:01:47 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+
+        int left = 0, right = height.size() - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int ans = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = max(leftMax, height[left]);
+                ans += max(0, leftMax - height[left]);
+            } else {
+                right--;
+                rightMax = max(rightMax, height[right]);
+                ans += max(0, rightMax - height[right]);
+            }
+        }
+        return ans;
+    }
+};
