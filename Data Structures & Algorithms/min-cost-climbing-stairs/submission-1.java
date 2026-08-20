1c89aebcea36957539e4fc38b291719d643ed9d6
Devang Srivastava <63438842+Blackeyeforreal@users.noreply.github.com>
2026-08-21 01:39:28 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+        int [] dp = new int [ cost.length+1]; 
+        int  n= cost.length;
+        for(int i = 0;i<=n;i++) dp[i]= Integer.MAX_VALUE;
+        dp[0]=0;
+        dp[1] =0;
+        for(int i =0 ;i <n;i++)
+        {
+            if ( i +1<=n){
+                dp[i+1 ] =Math.min(dp[i+1], dp[i] + cost[i]);
+            }
+             if ( i +2<=n){
+                dp[i+2 ] =Math.min(dp[i+2], dp[i] + cost[i]);
+            }
+        }
+        return dp[n];
+    }
+}
